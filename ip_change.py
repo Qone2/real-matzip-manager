75b9f0c1d40102610b1db6f7cb09b5553a0cdf56
qone2 <whrbdnjs33@naver.com>
2021-11-03 11:46:36 +0900

    공유기의 공인 ip를 바꾸는 스크립트
    asus 라우터에 셀레니움 사용

---
+++ ip_change.py
@@ -0,0 +1,66 @@
+import selenium.common.exceptions
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from traceback import format_exc
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import requests
+import time
+import platform
+import json
+import datetime
+import random
+import threading
+
+
+def ip_change():
+    chrome_option = Options()
+    # chrome_option.add_argument("headless")
+
+    driver = webdriver.Chrome(executable_path="./chromedriver.exe")
+
+    driver.get("http://192.168.0.1")
+    time.sleep(1)
+    with open("./graph_api_secret.json", 'r', encoding="UTF8") as f:
+        account = json.load(f)["router_account"]
+    driver.find_element(By.ID, "login_username").send_keys(account["id"])
+    time.sleep(1)
+    driver.find_element(By.NAME, "login_passwd").send_keys(account["pw"])
+    time.sleep(1)
+    driver.find_element(By.CSS_SELECTOR, ".button").click()
+    time.sleep(1)
+    driver.find_element(By.XPATH, "/html/body/table/tbody/tr/td[2]/div[1]/div/div[12]").click()
+    time.sleep(1)
+    change_mac_addr(driver)
+
+    try:
+        while driver.find_element(By.XPATH, "/html/body/form[2]/table/tbody/tr/td[3]/div/table/tbody/tr/td/table/tbody/tr/td/table[9]/tbody/tr[4]/td/div"):
+            change_mac_addr(driver)
+    except:
+        pass
+    time.sleep(10)
+
+def change_mac_addr(driver:WebDriver):
+    mac_addr = driver.find_element(By.NAME, "wan_hwaddr_x")
+    for _ in range(17):
+        time.sleep(0.1)
+        mac_addr.send_keys(Keys.BACKSPACE)
+    for _ in range(12):
+        time.sleep(0.1)
+        key_ascii = int(random.uniform(48, 57 + 5.99))
+        if key_ascii <= 57:
+            key = chr(key_ascii)
+        else:
+            key = chr(key_ascii + 39)
+        mac_addr.send_keys(key)
+    driver.find_element(By.XPATH, "/html/body/form[2]/table/tbody/tr/td[3]/div/table/tbody/tr/td/table/tbody/tr/td/div[7]/input").click()
+
+
+
+if __name__ == "__main__":
+    ip_change()
+    time.sleep(1000)
